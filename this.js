15254f4f28a3253aaeee7e12e4c12d86c742e5db
yesSwaroop <92199901+yesSwaroop@users.noreply.github.com>
2021-10-19 15:06:31 +0530

    Add files via upload

--- this.js
+++ this.js
@@ -4,7 +4,7 @@ function issuff(){
     setTimeout(()=>{
         var res = window.innerWidth;
         if(res<1100 && rescnt<2){
-            alert(":(\nMaximise window / View on Laptop or PC")
+            alert(":(\nMaximise window / View on Laptop or PC");
             rescnt+=1;
         }
     },100);  
@@ -13,6 +13,7 @@ window.addEventListener("resize",issuff);
 window.addEventListener("load",issuff);
 
 //Stack Height Initialization:
+var bactive = false;
 var hinc = document.getElementById("hinc");
 var hdec = document.getElementById("hdec");
 var hv = document.getElementById("hv");
@@ -22,23 +23,29 @@ var currheight = 0;
 var MAX_HEIGHT = -1;
 
 hinc.addEventListener("click",()=>{
-    if(currheight != 9){
+    if(currheight != 9 && bactive==false){
+        bactive = true;
         currheight += 1;
         hv.innerHTML = height[currheight];
+        bactive = false;
     }
 });
 
 hdec.addEventListener("click",()=>{
-    if(currheight!=0){
+    if(currheight!=0 && bactive==false){
+        bactive = true;
         currheight -= 1;
         hv.innerHTML = height[currheight];
+        bactive = false;
     }
 });
 
 seth.addEventListener("click",()=>{
-    if(MAX_HEIGHT==-1){
+    if(MAX_HEIGHT==-1 && bactive==false){
+        bactive = true;
         MAX_HEIGHT = hv.innerHTML;
         buildstack(MAX_HEIGHT);
+        bactive = false;
     }
 })
 
@@ -62,10 +69,9 @@ function buildstack(height){
 //Push:
 var pushb = document.getElementById("pushb");
 var firstEmpty = 1;
-var pushing = false;
 
 function push(){
-    pushing = true;
+    bactive = true;
     let level = MAX_HEIGHT;
     let fltr =  document.getElementById("fltr");
     let rec=[];
@@ -84,14 +90,72 @@ function push(){
     setTimeout(()=>{
         rec[firstEmpty].classList = "full";
         firstEmpty+=1;
-        pushing = false;
+        bactive = false;
     },(level-firstEmpty+1)*500);
 }
 pushb.addEventListener("click",()=>{
-    if(firstEmpty>MAX_HEIGHT)
+    if(firstEmpty>MAX_HEIGHT && !bactive && MAX_HEIGHT!=-1)
         popup("warn","Stack is full!!");
-    else if(!pushing)
+    else if(!bactive && MAX_HEIGHT!=-1)
         push();
+});
+
+//Pop:
+var popb = document.getElementById("ppm");
+function pop(){
+    bactive = true;
+    firstEmpty-=1;
+    let fltr =  document.getElementById("fltr");
+    let rec=[];
+    let time = 250;
+    for(let i=firstEmpty;i<=MAX_HEIGHT;i++){
+        rec[i] = document.getElementById(`r${i}`);
+    }
+    rec[firstEmpty].classList = "empty";
+    for(let i=firstEmpty+1;i<=MAX_HEIGHT;i++){
+        setTimeout(()=>{rec[i].classList = "full";},time);
+        time+=250;
+        setTimeout(()=>{rec[i].classList = "empty";},time);
+        time+=250;
+    }
+    setTimeout(()=>{
+        fltr.style.visibility = "visible";
+        setTimeout(()=>{fltr.style.visibility = "hidden";},300);
+        bactive = false;
+    },500*(MAX_HEIGHT-firstEmpty)+250); 
+    
+}
+popb.addEventListener("click",()=>{
+    if(firstEmpty==1 && !bactive && MAX_HEIGHT!=-1)
+        popup("warn","Stack is empty!!");
+    else if(!bactive && MAX_HEIGHT!=-1)
+        pop();
+})
+
+//isEmpty:
+var iEb = document.getElementById("iEb");
+iEb.addEventListener("click",()=>{
+    if(!bactive && MAX_HEIGHT!=-1){
+        bactive = true;
+        if(firstEmpty == 1)
+            popup("msg","isEmpty : true");
+        else    
+            popup("msg","isEmpty : false");
+        bactive = false;
+    }
+});
+
+//isFull:
+var iFb = document.getElementById("iFb");
+iFb.addEventListener("click",()=>{
+    if(!bactive && MAX_HEIGHT!=-1){
+        bactive = true;
+        if(firstEmpty > MAX_HEIGHT)
+            popup("msg","isFull : true");
+        else    
+            popup("msg","isFull : false");
+        bactive = false;
+    }
 });
 
 //Popup:
